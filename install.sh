a021792af54f2d5eabaedf90d2550351745b3daf
Alice <sousaku_0912@yahoo.co.jp>
2022-02-28 18:40:25 +0900

    first commit

---
+++ install.sh
@@ -0,0 +1,6 @@
+
+yarn add @nuxtjs/axios@5.13.1 axios@0.24.0 chart.js@2.9.4 firebase@9.6.6 firebaseui@6.0.0 fuse.js@6.5.3 sass@1.45.2 sass-loader@10 vuex@3.6.2
+yarn remove @nuxtjs/axios@5.13.1 axios@0.24.0 chart.js@2.9.4 firebase@9.6.6 firebaseui@6.0.0 fuse.js@6.5.3 sass@1.45.2 sass-loader@10 vuex@3.6.2
+yarn add -D  @babel/core@7.17.0 @babel/preset-env@7.16.11 @nuxtjs/eslint-config-typescript@8.0.0 @nuxtjs/eslint-module@3.0.2 babel-eslint@10.1.0 eslint@8.8.0 eslint-config-prettier@8.3.0 eslint-loader@4.0.2 eslint-plugin-nuxt@3.1.0 eslint-plugin-prettier@4.0.0 jest-serializer-vue@2.0.2 prettier@2.5.1
+yarn remove -D  @babel/core@7.17.0 @babel/preset-env@7.16.11 @nuxtjs/eslint-config-typescript@8.0.0 @nuxtjs/eslint-module@3.0.2 babel-eslint@10.1.0 eslint@8.8.0 eslint-config-prettier@8.3.0 eslint-loader@4.0.2 eslint-plugin-nuxt@3.1.0 eslint-plugin-prettier@4.0.0 jest-serializer-vue@2.0.2 prettier@2.5.1
+
